7d13c4f354368be89527ee92313456ed53ef2f26
yasmin-honda <yasmin.honda@unifesp.br>
2024-11-05 15:13:41 -0300

    Create 2729.c

---
+++ listas/2729.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+struct no{
+    char nome[20];
+    struct no *prox;
+};
+
+typedef struct lista{
+    struct no *inicio;
+}compras;
+
+int main(){
+    int n;
+    
+    scanf("%d",&n);
+    lista 
+}
