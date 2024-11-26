1547064383725196b226b8d04bd0ae032817542e
yasmin-honda <yasmin.honda@unifesp.br>
2024-11-25 22:55:15 -0300

    Update 2381.c

--- ordenação/2381.c
+++ ordenação/2381.c
@@ -1,66 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
- //verificar se a letra é maior ou menor do que o do nome já existente na lista e colocar na posição certa
-// ou colocar tudo na ordem escrita e depois ordenar em ordem alfabética
-typedef struct l{
-    char nome[20];
-    struct l *prox;
-}lista;
+// fazer um programa que receba uma lista de nomes, coloque em ordem alfabética e identifique qual nome corresponde ao número sorteado
+#include<stdio.h> 
+#include<string.h>
 
-void inserir_fim(lista **chamada, char v[]){
-    lista *nome1,*aux;
-
-    aux=*chamada;
-    nome1=malloc(sizeof(lista));
-    if(aux->prox==NULL){
-        for(int i=0;v[i]!='/0';i++){
-            nome1->nome[0]=v[0];
-        }
-        nome1->prox=NULL;
-        aux->prox=nome1;
-    }
-    else{
-        while(aux->prox){
-            aux=aux->prox;
-        }
-        for(int i=0;v[i]!='/0';i++){
-            nome1->nome[0]=v[0];
-        }
-        nome1->prox=NULL;
-        aux->prox=nome1;
-    }
-
+void merge_sort(char v[][20],int inicio, int fim){
+        int meio; //tentaremos sem o passo de inicio<fim pq na minha cabeça nn precisa
+        meio=(inicio+fim)/2;
+        merge_sort(v,inicio,meio);
+        merge_sort(v,meio+1,fim);
+        merge(v,inicio,meio,fim);
 }
+void merge(char v[][20],int inicio, int meio, int fim){
+    int i=inicio, j=meio+1, k=0;
+    char *temp[fim - inicio+1];
 
-void ordenar(lista **chamada){ //ordena a lista em ordem alfabetica; usar a estratégia de numero crescente?
-    lista *aux1,*aux2;
-
-    aux1=chamada->prox;
-    aux2=(chamada->prox)->prox;
-    while(aux1->prox){
-        if(aux1->nome[0]>aux2->nome[0]){
-            lista *aux3;
-
-            aux3=aux1->prox;
-            aux1->prox=aux2->prox;
-            aux2->prox=aux3;
+    while (i<=meio && j<=fim){
+        if(strcmp(v[i], v[j]) < 0){
+            temp[k++] = v[i++];
         }
+        else temp[k++] = v[j++];
+    }
+    while (i<=meio) temp[k++] = v[i++];
+    while (j<=fim) temp[k++] = v[j++];
+
+    for (i=inicio; i<=fim; i++){
+        strncpy(v[i],temp[i-inicio],strlen(temp[i-inicio]));
     }
 }
-
-//criar função que verifica qual o nome correspondente ao numero sorteado
-//imprimir o nome
 
 int main(){
     int n,k;
-    char nomes[20];
-    lista *cabeca;
 
-    cabeca=malloc(sizeof(lista));
-    cabeca->prox=NULL;
     scanf("%d %d",&n,&k);
-    for(int i=0;i<n;i++){
-        gets(nomes);
-        inserir_fim(&cabeca,nomes);
+    char nomes[n][20];
+    for(int i=-1;i<n;i++){ //por que ele não pega a palavra da linha 0?
+        gets(nomes[i]);
     }
+    merge_sort(nomes[n][20],0,n);
+    printf("%s\n",nomes[k]);
 }
