de3be606f7febdf00e466f6f000d9b0c3c79f567
yasmin-honda <yasmin.honda@unifesp.br>
2024-11-28 21:04:36 -0300

    Update 2381.c

--- ordenação/2381.c
+++ ordenação/2381.c
@@ -1,47 +1,43 @@
 // fazer um programa que receba uma lista de nomes, coloque em ordem alfabética e identifique qual nome corresponde ao número sorteado
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void merge_sort(char *v[],int inicio, int fim){
-        int meio;
-        meio=(inicio+fim)/2;
-        merge_sort(v,inicio,meio);
-        merge_sort(v,meio+1,fim);
-        merge(v,inicio,meio,fim);
-}
-void merge(char *v[],int inicio, int meio, int fim){
-    int i=inicio, j=meio+1, k=0;
-    char *temp[fim - inicio+1];
+void merge(char v[][22], int inicio, int meio, int fim) {
+    int i = inicio, j = meio + 1, k = 0;
+    char temp[fim - inicio + 1][22];
 
-    while (i<=meio && j<=fim){
-        if(strcmp(v[i], v[j]) < 0){
-            temp[k++] = v[i++];
-        }
-        else temp[k++] = v[j++];
+    while (i <= meio && j <= fim) {
+        if (strcmp(v[i], v[j]) < 0) strcpy(temp[k++], v[i++]);
+        else strcpy(temp[k++], v[j++]);
     }
-    while (i<=meio) temp[k++] = v[i++];
-    while (j<=fim) temp[k++] = v[j++];
-
-    for (i=inicio; i<=fim; i++){
-        free(v[i]);
-        strncpy(v[i],temp[i-inicio], strlen(temp[i-inicio]));
+    while (i<=meio) strcpy(temp[k++], v[i++]);
+    while(j<=fim) strcpy(temp[k++], v[j++]);
+    for (i = inicio; i <= fim; i++) {
+        strcpy(v[i], temp[i - inicio]);
     }
 }
 
-int main(){
-    int n,k;
+void merge_sort(char v[][22], int inicio, int fim) {
+    if(inicio<fim){ //define o caso de parada da função recursiva, ou seja, quando o inicio se iguala ao fim
+        int meio = (inicio + fim) / 2;
+        merge_sort(v, inicio, meio);
+        merge_sort(v, meio + 1, fim);
+        merge(v, inicio, meio, fim);
+    }
+}
 
-    scanf("%d %d",&n,&k);
-    char *nomes[n];
-    getchar();
-    for(int i=0;i<n;i++){ 
-        nomes[i]=malloc(20*sizeof(char));
-        gets(nomes[i]);
+int main() {
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+    getchar(); //sem o getchar o programa armazena a primeira quebra de linha como a informação da primeira string
+    char nomes[n][22]; //gambiarra para permitir que o número maximo de caracteres inseridos seja 20
+
+    for (int i=0; i<n;i++) {
+        fgets(nomes[i], 22, stdin);
+        nomes[i][strcspn(nomes[i], "\n")] = '\0';
+
     }
-    merge_sort(nomes,0,n-1);
-    printf("%s\n",nomes[k]);
-    for(int i=0;i<n;i++){
-        free(nomes[i]);
-    }
+    merge_sort(nomes, 0, n-1);
+    printf("%s\n", nomes[k-1]);
 }
