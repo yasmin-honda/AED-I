c432a0130b31abc785c9ca0e7633d41b3e0883d1
yasmin-honda <yasmin.honda@unifesp.br>
2025-01-14 20:29:48 -0300

    Update 1855.c

--- grafos/1855.c
+++ grafos/1855.c
@@ -1,48 +1,50 @@
+/*Código que recebe 2 inteiros com a largura e altura de uma matriz, respectivamente 
+Identifica se a matriz recebida é um mapa válido (leva a um baú "*") ou inválido (não dá em nada "!")
+*/
 #include<stdio.h>
+#include<string.h>
 
 int main(){
     char matriz[100][100];
-    int x,y,i=0,j=0,cont=0;
-
-    scanf("%d %d",&x,&y);
-    for(int i=0;i<y;i++){
-        for(int j=0;j<x;j++){
-            scanf(" %c",matriz[i][j]);
+    int x, y, i=0, j=0,aux=0;
+    
+    scanf("%d %d", &x, &y);
+    for(int i = 0; i < y; i++) {
+        for(int j = 0; j < x; j++) {
+            scanf(" %c", &matriz[i][j]);
+            if(matriz[i][j] == '*') {
+                aux++; 
+            }
         }
     }
-    while(matriz[i][j]!='*'){
-        printf("OI");
-        if(matriz[i][j]=='>'){
-            j++;
-            while(matriz[i][j]=='.')j++;
-            cont++;
+    if(aux == 1) {
+        while(i >= 0 && i < y && j >= 0 && j < x) {
+            if(matriz[i][j] == '*') {
+                printf("*\n");
+                break;
+            }
+            if(matriz[i][j] == '>') {
+                j++;
+                while(matriz[i][j] == '.') j++;
+            } 
+            else if(matriz[i][j] == '<') {
+                j--;
+                while(matriz[i][j] == '.') j--; 
+            } 
+            else if(matriz[i][j] == '^') {
+                i--;
+                while(matriz[i][j] == '.') i--; 
+            } 
+            else if(matriz[i][j] == 'v') {
+                i++;
+                while(matriz[i][j] == '.') i++; 
+            } 
+            if(matriz[i][j] != '*' && matriz[i][j] != '>' && matriz[i][j] != '<' && matriz[i][j] != '^' && matriz[i][j] != 'v') { //se percorreu todas as direções e não achou um baú, então é inválido
+                printf("!\n");
+                break;
+            }
         }
-        if(matriz[i][j]=='<'){
-            j--;
-            while(matriz[i][j]=='.')j--;
-            cont++;
-        }
-        if(matriz[i][j]=='^'){
-            i--;
-            while(matriz[i][j]=='.')i--;
-            cont++;
-        }
-        if(matriz[i][j]=='v'){
-            i++;
-            while(matriz[i][j]=='.')i++;
-            cont++;
-        }
-        if(matriz[i][j]=='*'){
-            cont++;
-            printf("*\n");
-            break;
-        }
-        if(cont>=x*y){
-            printf("!\n");
-            break;
-        }
-    }
-
+    } 
+    else{
+        printf("!\n"); 
 }
-
-
