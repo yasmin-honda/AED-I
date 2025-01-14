265f8a45766b0081a4b7622e8d2f21003d7ded3f
yasmin-honda <yasmin.honda@unifesp.br>
2025-01-13 22:02:27 -0300

    Update 1069.c

--- listas/1069.c
+++ listas/1069.c
@@ -90,3 +90,45 @@ int main(){
         printf("%d\n", diamantes[i]);
     }
 }
+
+SEGUNDA ABORDAGEM!!
+
+/* Cria um programa que recebe um inteiro n indicando o numero de conjuntos a serem testados, que serão os nós da lista
+Guarda as informações do conjunto em uma string e analisa quantos diamantes podem ser formados
+Repete o processo para os nós subsequentes da lista
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 1000
+
+typedef struct no{
+    char string[MAX];
+    struct no *prox;
+}No;
+
+void inserir_no(No **lista,char simbolo){
+    No *novo=malloc(sizeof(No)),*aux=*lista;
+
+    novo->valor=num;
+    novo->prox=NULL;
+    if(*lista==NULL){
+        *lista=novo;
+    }
+    else{
+        while(aux->prox)aux=aux->prox;
+        aux->prox=novo;
+    }
+}
+
+int main(){
+    No *lista=malloc(sizeof(No));
+    int n;
+    char vetor[MAX];
+
+    lista=NULL; //a lista será sem cabeça porque não há necessidade de um marcador inicial neste caso
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        gets(vetor);
+        inserir_no(&lista,vetor);
+    }
+}
