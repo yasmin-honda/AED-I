0a4303b3b48859c048bdd8d944c8a47cf29951e5
yasmin-honda <yasmin.honda@unifesp.br>
2025-01-21 19:29:28 -0300

    Update 1897.c

--- ponteiros e recursão/1897.c
+++ ponteiros e recursão/1897.c
@@ -1,22 +1,22 @@
 //dado um numero n e um numero m, devolve a quantidade minima de operações que convertem n em m
 #include<stdio.h>
 
-int operacoes(int n, int m) {
-    int minimo=1,op[6],menor;
+int operacoes(int n, int m, int num) {
+    int op[6],minimo;
 
-    if(n*2 == m || n*3 == m || n/2 == m || n/3 == m || n+7 == m || n-7 == m) return minimo;
+    if(n*2 == m || n*3 == m || n/2 == m || n/3 == m || n+7 == m || n-7 == m || num>10) return num;
     else{
-        op[0]=operacoes(n*2,m);
-        op[1]=operacoes(n*3,m);
-        op[2]=operacoes(n/2,m);
-        op[3]=operacoes(n/3,m);
-        op[4]=operacoes(n+7,m);
-        op[5]=operacoes(n-7,m);
-        menor=op[0];
+        op[0]=operacoes(n*2,m,num+1);
+        op[1]=operacoes(n*3,m,num+1);
+        op[2]=operacoes(n/2,m,num+1);
+        op[3]=operacoes(n/3,m,num+1);
+        op[4]=operacoes(n+7,m,num+1);
+        op[5]=operacoes(n-7,m,num+1);
+        minimo=op[0];
         for(int i=1;i<6;i++){
-            if(op[i]<menor) menor=op[i];
+            if(op[i]<=minimo) minimo=op[i];
         }
-        return menor;
+        return minimo;
     }
 }
 
@@ -24,5 +24,5 @@ int main() {
     int n, m;
 
     scanf("%d %d", &n, &m);
-    printf("%d\n", operacoes(n, m));
+    printf("%d\n", operacoes(n, m, 1));
 }
