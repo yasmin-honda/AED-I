9c034af68879d26dba6e18bc8a422e1dac2b240b
yasmin-honda <yasmin.honda@unifesp.br>
2025-02-02 14:53:42 -0300

    Update 1069.c

--- listas/1069.c
+++ listas/1069.c
@@ -1,134 +1,46 @@
 /* Cria um programa que recebe um inteiro n indicando o numero de conjuntos a serem testados
-Guarda as informações do conjunto em uma lista e analisa quantos diamantes podem ser formados
-"Reseta" as informações da lista e repete para cada n linha
+Identifica a formação de um par '<>' e o substitui por x para que seja contado corretamente
 */
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX 1000
+#include <stdio.h>
+#include <string.h>
 
-typedef struct l{
-    char valor;
-    struct l *prox;
-}no;
+typedef unsigned short us;
 
-void inserir(no *head, char simbolo){
-    no *novo=malloc(sizeof(no)),*aux=head;
+us diamonds(char *);
 
-    if(novo){
-        novo->prox=NULL;
-        novo->valor=simbolo;
-    }
-    else printf("Erro de alocação");
-    while(aux->prox) aux=aux->prox;
-    aux->prox=novo;
+us diamonds(char *string){
+
+	short x = 0;
+	us qtsDiamonds = 0;
+
+	us i, j;
+	for (i = 0; string[i]; ++i){
+		if (string[i] != '<')continue;
+    
+		for (j = i + 1; string[j]; ++j){
+			if (string[j] == '>'){
+				++qtsDiamonds;
+				string[i] = 'x';
+				string[j] = 'x';
+				break;
+			}
+		}
+	}
+	return qtsDiamonds;
 }
 
-void retirar(no *head,no *marcador){
-    no *aux=head->prox,*aux2;
-    while(aux!=marcador){
-      aux2=aux;
-      aux=aux->prox;
-    }
-    aux2->prox=aux->prox;
-}
+int main (){
+	us n;
+	int lixo;
+	char string[1005];
 
-int contar_diamante(no *head, int cont){ //por algum motivo essa função não está retornando e o erro no código começa aqui
-    int num=0;
-    no *aux,*marcador1,*marcador2;
+	scanf("%hu", &n);
+	scanf("%d", &lixo);
 
-    for(int j=0;j<cont;j++){
-        aux=head->prox;
-        while(aux->valor!='<') aux=aux->prox;
-        marcador1=aux;
-        while(aux->valor!='>'){
-            if(aux->prox==NULL) break;
-            aux=aux->prox;
-        }
-        if(aux->valor!='>') break;
-        marcador2=aux;
-        if(marcador1->valor=='<' && marcador2->valor=='>'){
-            num++;
-            retirar(head,marcador1);
-            retirar(head,marcador2);
-        }
-    }
-    return num;
-}
-
-void limpar(no *lista) {
-    no *aux=lista->prox,*temp;
-
-    while(aux){
-        temp=aux;
-        aux=aux->prox;
-        free(temp);
-    }
-}
-
-
-int main(){
-    no *lista=malloc(sizeof(no));
-    int n,diamantes[MAX],cont=0;
-    char c=' ';
-
-    scanf("%d",&n);
-    lista->prox=NULL;
-    for(int i=0;i<n;i++){
-        //analisa uma linha e conta quantos diamantes
-        getchar();
-        while(c!='\n'){
-           scanf("%c",&c);
-           inserir(lista,c);
-           if(c=='<') cont++;
-        }
-        diamantes[i]=contar_diamante(lista,cont);
-        printf("oi2");
-        limpar(lista);
-        cont=0; //zera a contagem quando a lista for resetar
-    }
-    for(int i=0;i<n;i++){
-        printf("%d\n", diamantes[i]);
-    }
-}
-
-SEGUNDA ABORDAGEM!!
-
-/* Cria um programa que recebe um inteiro n indicando o numero de conjuntos a serem testados, que serão os nós da lista
-Guarda as informações do conjunto em uma string e analisa quantos diamantes podem ser formados
-Repete o processo para os nós subsequentes da lista
-*/
-#include<stdio.h>
-#include<stdlib.h>
-#define MAX 1000
-
-typedef struct no{
-    char string[MAX];
-    struct no *prox;
-}No;
-
-void inserir_no(No **lista,char simbolo){
-    No *novo=malloc(sizeof(No)),*aux=*lista;
-
-    novo->valor=num;
-    novo->prox=NULL;
-    if(*lista==NULL){
-        *lista=novo;
-    }
-    else{
-        while(aux->prox)aux=aux->prox;
-        aux->prox=novo;
-    }
-}
-
-int main(){
-    No *lista=malloc(sizeof(No));
-    int n;
-    char vetor[MAX];
-
-    lista=NULL; //a lista será sem cabeça porque não há necessidade de um marcador inicial neste caso
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        gets(vetor);
-        inserir_no(&lista,vetor);
-    }
+	while (n--){
+		fgets(string, 1005, stdin);
+		string[strlen(string) - 1] = '\0';
+		printf("%hu\n", diamonds(string));
+		memset(string, 0, sizeof(string));
+ 	}
 }
