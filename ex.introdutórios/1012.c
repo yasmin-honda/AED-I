874acd14f792248fadc8f65d60dd12ac7e63ce70
yasmin-honda <yasmin.honda@unifesp.br>
2025-02-10 00:43:08 -0300

    Create 1012.c

---
+++ ex.introdutórios/1012.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    double a,c,b;
+    double tri,qua,tra,ret,cir;
+    
+    scanf("%lf %lf %lf",&a,&b,&c);
+    tri=(a*c)/2;
+    cir=3.14159*(c*c);
+    tra=(a+b)*c/2;
+    qua=b*b;
+    ret=a*b;
+    printf("TRIANGULO: %.3lf\n",tri);
+    printf("CIRCULO: %.3lf\n",cir);
+    printf("TRAPEZIO: %.3lf\n",tra);
+    printf("QUADRADO: %.3lf\n",qua);
+    printf("RETANGULO: %.3lf\n",ret);
+}
